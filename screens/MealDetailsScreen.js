f1099bb415d3d542811acf9e8265f42072f65310
nel11326 <lenor11326@hotmail.com>
2024-08-20 07:35:47 +0800

    Icon(add): adding Icon button to a header

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,17 +1,11 @@
 import { useLayoutEffect } from "react";
-import {
-  Image,
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  Button,
-} from "react-native";
+import { Image, StyleSheet, Text, View, ScrollView } from "react-native";
 
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
+import IconButton from "../components/IconButton";
 
 function MealDetailsScreen({ route, navigation }) {
   const mealId = route.params.mealId;
@@ -25,7 +19,13 @@ function MealDetailsScreen({ route, navigation }) {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => {
-        return <Button title="Tap me" onPress={headerButtonPressHandler} />;
+        return (
+          <IconButton
+            onPress={headerButtonPressHandler}
+            icon={"star"}
+            color={"white"}
+          />
+        );
       },
     });
   }, [navigation, headerButtonPressHandler]);
